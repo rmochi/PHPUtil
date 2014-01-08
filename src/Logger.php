d02bbe3a2feddf50dd0d7cb3099d675e07de5e3b
rmochi <rm_mugen@yahoo.co.jp>
2014-01-09 04:26:48 +0900

    Update Logger.php

--- src/Logger.php
+++ src/Logger.php
@@ -3,28 +3,28 @@ namespace rmochi\PHPUtil;
 
 class Logger
 {
-	const LOG_LEVEL_DEV			= 0;
-	const LOG_LEVEL_PROD_CHECK	= 1;
-	const LOG_LEVEL_PROD		= 2;
+	const LOG_LEVEL_DEV         = 0;
+	const LOG_LEVEL_PROD_CHECK  = 1;
+	const LOG_LEVEL_PROD        = 2;
 
 	protected static $log_level = 2;
 
 	protected static $log_level_config = array(
 		'd' => array(
-			'level'	=> 0,
-			'text'	=> 'DEBUG',
+			'level' => 0,
+			'text'  => 'DEBUG',
 		),
 		'i' => array(
-			'level'	=> 1,
-			'text'	=> 'INFO',
+			'level' => 1,
+			'text'  => 'INFO',
 		),
 		'e' => array(
-			'level'	=> 2,
-			'text'	=> 'ERROR',
+			'level' => 2,
+			'text'  => 'ERROR',
 		),
 		'c' => array(
-			'level'	=> 2,
-			'text'	=> 'CRIT',
+			'level' => 2,
+			'text'  => 'CRIT',
 		),
 	);
 
